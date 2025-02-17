e5839926a8861022e98780204bbee279ee664220
BlaskVN <124863795+BlaskVN@users.noreply.github.com>
2025-02-17 18:28:39 +0700

    Change to ping pong simple

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, MessageFlags } = require('discord.js');
 
 module.exports = {
 	cooldown: 1,
@@ -6,6 +6,6 @@ module.exports = {
 		.setName('ping')
 		.setDescription('Replies with Pong!'),
 	async execute(interaction) {
-		await interaction.reply({ content: 'Secret Pong!', ephemeral: true });
+		await interaction.reply('Pong!');
 	},
 };
